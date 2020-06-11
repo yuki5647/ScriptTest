160e453d1de349c6bcddda3ca102cc45f49faedf
yukitosi <t.yuki.0527@gmail.com>
2020-06-11 18:47:54 +0900

    ScriptTestプロジェクト　二度目コミット
    配列を順序、逆順に表示させました。

---
+++ Assets/Test.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Test : MonoBehaviour {
+
+   
+
+    
+
+    // Use this for initialization
+    void Start() {
+
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        for (int a = 0; a < array.Length; a++) {
+
+            Debug.Log(array[a]);
+
+        }
+
+        
+
+        for (int a = 4; a >=0 ; a--)
+        {
+
+            Debug.Log(array[a]);
+        }
+       
+
+        }
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+ }
